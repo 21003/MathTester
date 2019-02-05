b2668d8135a5d4089074a2cd646555aa11832c6b
Jacob Johnston <jacob.johnston.2022@student.lhschools.org>
2019-02-05 14:14:05 -0500

    added int firstNumber ans secondNumber

--- src/MathTester.java
+++ src/MathTester.java
@@ -3,7 +3,10 @@ public class MathTester {
     public static void main(String[] args){
         String firstWord;
         String secondWord;
-      
+        int firstNumber, secondNumber;
+
+       
+
 
 
 
